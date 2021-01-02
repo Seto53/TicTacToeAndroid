81074cbc8717e8a2405ca1b3c5b38dd6aca986ff
Setomaster <afarah500@gmail.com>
2021-01-02 15:35:44 -0500

    add player vs computer

--- app/src/main/java/com/setomaster/tictactoe/MainActivity.java
+++ app/src/main/java/com/setomaster/tictactoe/MainActivity.java
@@ -12,82 +12,116 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     TicTacToeGame game = new TicTacToeGame();
-    Player[] players = new Player[2];
     Random rand = new Random();
     int firstPlayer = rand.nextInt(2);
+    int gameMode = 0;
+    ImageView cell0;
+    ImageView cell1;
+    ImageView cell2;
+    ImageView cell3;
+    ImageView cell4;
+    ImageView cell5;
+    ImageView cell6;
+    ImageView cell7;
+    ImageView cell8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        cell0 = findViewById(R.id.cell0);
+        cell1 = findViewById(R.id.cell1);
+        cell2 = findViewById(R.id.cell2);
+        cell3 = findViewById(R.id.cell3);
+        cell4 = findViewById(R.id.cell4);
+        cell5 = findViewById(R.id.cell5);
+        cell6 = findViewById(R.id.cell6);
+        cell7 = findViewById(R.id.cell7);
+        cell8 = findViewById(R.id.cell8);
+    }
+
+    public void computerPlay() {
+        Random rand = new Random();
+        int rand_1;
+        do {
+            rand_1 = rand.nextInt(game.getLines() * game.getColumns());
+        } while (game.valueAt(rand_1) != CellValue.EMPTY);
+        game.play(rand_1);
+        update();
+    }
+
+    public void nextPlay() {
+        update();
+        if (gameMode == 0) {
+            computerPlay();
+        }
     }
 
     public void cell0(View v) {
-        if(game.valueAt(0) == CellValue.EMPTY){
+        if (game.valueAt(0) == CellValue.EMPTY) {
             game.play(0);
-            update();
+            nextPlay();
         }
     }
+
     public void cell1(View v) {
-        if(game.valueAt(1) == CellValue.EMPTY){
+        if (game.valueAt(1) == CellValue.EMPTY) {
             game.play(1);
-            update();
+            nextPlay();
         }
     }
+
     public void cell2(View v) {
-        if(game.valueAt(2) == CellValue.EMPTY){
+        if (game.valueAt(2) == CellValue.EMPTY) {
             game.play(2);
-            update();
+            nextPlay();
         }
     }
+
     public void cell3(View v) {
-        if(game.valueAt(3) == CellValue.EMPTY){
+        if (game.valueAt(3) == CellValue.EMPTY) {
             game.play(3);
-            update();
+            nextPlay();
         }
     }
+
     public void cell4(View v) {
-        if(game.valueAt(4) == CellValue.EMPTY){
+        if (game.valueAt(4) == CellValue.EMPTY) {
             game.play(4);
-            update();
+            nextPlay();
         }
     }
+
     public void cell5(View v) {
-        if(game.valueAt(5) == CellValue.EMPTY){
+        if (game.valueAt(5) == CellValue.EMPTY) {
             game.play(5);
-            update();
+            nextPlay();
         }
     }
+
     public void cell6(View v) {
-        if(game.valueAt(6) == CellValue.EMPTY){
+        if (game.valueAt(6) == CellValue.EMPTY) {
             game.play(6);
-            update();
+            nextPlay();
         }
     }
+
     public void cell7(View v) {
-        if(game.valueAt(7) == CellValue.EMPTY){
+        if (game.valueAt(7) == CellValue.EMPTY) {
             game.play(7);
-            update();
+            nextPlay();
         }
     }
+
     public void cell8(View v) {
-        if(game.valueAt(8) == CellValue.EMPTY){
+        if (game.valueAt(8) == CellValue.EMPTY) {
             game.play(8);
-            update();
+            nextPlay();
         }
     }
 
     private void update() {
-        ImageView cell0 = findViewById(R.id.cell0);
-        ImageView cell1 = findViewById(R.id.cell1);
-        ImageView cell2 = findViewById(R.id.cell2);
-        ImageView cell3 = findViewById(R.id.cell3);
-        ImageView cell4 = findViewById(R.id.cell4);
-        ImageView cell5 = findViewById(R.id.cell5);
-        ImageView cell6 = findViewById(R.id.cell6);
-        ImageView cell7 = findViewById(R.id.cell7);
-        ImageView cell8 = findViewById(R.id.cell8);
-
         if (game.valueAt(0) == CellValue.X) {
             cell0.setImageResource(R.drawable.x);
         } else if (game.valueAt(0) == CellValue.O) {
@@ -133,39 +167,28 @@ public class MainActivity extends AppCompatActivity {
         } else if (game.valueAt(8) == CellValue.O) {
             cell8.setImageResource(R.drawable.o);
         }
-        if(game.getGameState() != GameState.PLAYING){
-            if(game.getGameState() == GameState.DRAW){
+        if (game.getGameState() != GameState.PLAYING) {
+            if (game.getGameState() == GameState.DRAW) {
                 Toast.makeText(getApplicationContext(), "DRAW", Toast.LENGTH_LONG).show();
-            }
-            else if(game.getGameState() == GameState.XWIN){
+            } else if (game.getGameState() == GameState.XWIN) {
                 Toast.makeText(getApplicationContext(), "XWIN", Toast.LENGTH_LONG).show();
-            }
-            else if(game.getGameState() == GameState.OWIN){
+            } else if (game.getGameState() == GameState.OWIN) {
                 Toast.makeText(getApplicationContext(), "OWIN", Toast.LENGTH_LONG).show();
             }
             game = new TicTacToeGame();
             reset();
         }
-
     }
-    private void reset(){
-        ImageView cell0 = findViewById(R.id.cell0);
+
+    private void reset() {
         cell0.setImageResource(R.drawable.empty);
-        ImageView cell1 = findViewById(R.id.cell1);
         cell1.setImageResource(R.drawable.empty);
-        ImageView cell2 = findViewById(R.id.cell2);
         cell2.setImageResource(R.drawable.empty);
-        ImageView cell3 = findViewById(R.id.cell3);
         cell3.setImageResource(R.drawable.empty);
-        ImageView cell4 = findViewById(R.id.cell4);
         cell4.setImageResource(R.drawable.empty);
-        ImageView cell5 = findViewById(R.id.cell5);
         cell5.setImageResource(R.drawable.empty);
-        ImageView cell6 = findViewById(R.id.cell6);
         cell6.setImageResource(R.drawable.empty);
-        ImageView cell7 = findViewById(R.id.cell7);
         cell7.setImageResource(R.drawable.empty);
-        ImageView cell8 = findViewById(R.id.cell8);
         cell8.setImageResource(R.drawable.empty);
     }
 }
